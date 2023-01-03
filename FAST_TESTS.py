8850caff5decb1294f1780c7795415ad941f814a
rodfarah <digofarah@gmail.com>
2023-01-03 17:12:50 -0300

    simple test file

--- FAST_TESTS.py
+++ FAST_TESTS.py
@@ -1,3 +1,8 @@
-class SpaceAge:
-    def __init__(self, seconds):
-        pass
+l1 = [1, 2, 3, 4]
+l2 = ['a', 'b', 'c']
+
+result = [None]*(len(l1)+len(l2))
+result[::2] = l1
+result[1::2] = l2
+
+print(result)
