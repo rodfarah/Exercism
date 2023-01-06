10ca6ecd14efd54a72e372b2f9cf742671592113
rodfarah <digofarah@gmail.com>
2023-01-06 12:29:27 -0300

    4 iterarions, I was mentored in exercism

--- triangle.py
+++ triangle.py
@@ -1,32 +1,19 @@
-def is_triangle(sides):
-    a, b, c = sides
-    if a == 0 or b == 0 or c == 0:
-        return False
-    elif a + b >= c and b + c >= a and a + c >= b:
-        return True
-    else:
-        return False
+def is_triangle(func):
+    def wrap_func(sides):
+        return 0 not in sides and max(sides) <= sum(sides) - max(sides) and func(sides)
+    return wrap_func
 
 
+@is_triangle
 def equilateral(sides):
-    if is_triangle(sides) == False:
-        return False
-    elif len(set(sides)) == 1:
-        return True
-    return False
+    return len(set(sides)) == 1
 
 
+@is_triangle
 def isosceles(sides):
-    if is_triangle(sides) == False:
-        return False
-    elif 1 <= len(set(sides)) <= 2:
-        return True
-    return False
+    return 1 <= len(set(sides)) <= 2
 
 
+@is_triangle
 def scalene(sides):
-    if is_triangle(sides) == False:
-        return False
-    if len(set(sides)) == 3:
-        return True
-    return False
+    return len(set(sides)) == 3
