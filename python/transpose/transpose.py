22696ca10636326e13951f6f2e131431501f73c7
Rodrigo Farah <digofarah@gmail.com>
2023-04-20 14:21:47 -0300

    first commit to secondsolution

--- python/transpose/transpose.py
+++ python/transpose/transpose.py
@@ -1,22 +1,23 @@
 def transpose(*lines: str) -> str:
     """Transpose a string input."""
     lines = "\n".join(lines)
+    print(lines, "\n")
     splited_lines = lines.split("\n")
+    print(splited_lines, "\n")
     max_lenght = max([len(line) for line in splited_lines])
 
     # fill out any symbol (~) to end of lines in order to keep them with the same lenght
-    equal_length = []
-    for line in splited_lines:
-        equal_length.append(line + (max_lenght - len(line)) * "~")
+    equal_length = [line + (max_lenght - len(line)) * "~" for line in splited_lines]
+    print(equal_length, "\n")
 
     # create a list with transposed lines
-    each_one = []
     transposed_list = []
     for n in range(0, max_lenght):
+        each_one = []
         for line in equal_length:
             each_one.append(line[n])
         transposed_list.append("".join(each_one))
-        each_one = []
+    print(transposed_list, "\n")
 
     # remove symbol (~) from end of lines, keeping eventual original blank spaces at the end of lines
     for n in range(len(transposed_list)):
@@ -25,8 +26,11 @@ def transpose(*lines: str) -> str:
                 break
             else:
                 transposed_list[n] = transposed_list[n].removesuffix("~")
-                continue
+                
 
-    # if in the middle of a line, convert symbol (~) into a blank space
+    # if there is a symbol (~) in the middle of a line, convert it into a blank space
     result = [line.replace("~", " ") for line in transposed_list]
     return "\n".join(result)
+
+
+print(transpose("ABCDEF", "1234"))
