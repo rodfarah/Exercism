dadbd7d98685989ad5b1afebb8d60acb91bf6e45
Rodrigo Farah <digofarah@gmail.com>
2023-04-25 11:28:31 -0300

    Mentored by MatthijsBlom, great improvement

--- python/transpose/transpose.py
+++ python/transpose/transpose.py
@@ -1,41 +1,34 @@
+from itertools import zip_longest
+
+
+def no_terminal_none(any_list: list) -> list:
+    """Extract eventual terminal None s from a list"""
+    while any_list and any_list[-1] is None:
+        any_list.pop()
+    return any_list
+
+
 def transpose(*lines: str) -> str:
     """Transpose a string input."""
     lines = "\n".join(lines)
     splited_lines = lines.split("\n")
-    max_len = max([len(x) for x in splited_lines])
 
-    # Insert None(s) and create equal length list
-    equal_len = []
-    for line in splited_lines:
-        if len(line) < max_len:
-            each_line = [char for char in line]
-            for n in range(max_len - len(line)):
-                each_line.append(None)
-            equal_len.append(each_line)
-        else:
-            equal_len.append([char for char in line])
-    
-    # Create a list with characters combined by same indexes
-    idx = 0
-    combined_list = []
-    while idx < max_len:
-        each_comb = []
-        for lst in equal_len:
-            each_comb.append(lst[idx])
-        combined_list.append(each_comb)
-        idx += 1
-    
-    # Delete None if situated in the end
-    no_last_none = []
-    for comb in combined_list:
-        while comb and comb[-1] == None:
-            comb.pop()
-        no_last_none.append(comb)
-    
-    # Convert residual None's into blank spaces
-    none_to_space = []
-    for comb in no_last_none:
-        comb = [" " if char is None else char for char in comb]
-        none_to_space.append(comb)
+    # Isolate each character from each line and put them inside respective lists
+    splited_lines_lists = [list(item) for item in splited_lines]
+
+    # Transpose and combine isolated characters into tuples, already with Nones (if aplicable)
+    equal_len = list(zip_longest(*splited_lines_lists))
+
+    # Convert list of tuples into list of lists
+    equal_len = [list(item) for item in equal_len]
+
+    # Delete None s if situated at the end
+    no_last_none = [no_terminal_none(lst) for lst in equal_len]
+
+    # Convert residual None s into blank spaces
+    none_to_space = [[" " if char is None else char for char in comb]
+                     for comb in no_last_none]
+
+    # Final answer
     joined = ["".join(lst) for lst in none_to_space]
     return "\n".join(joined)
