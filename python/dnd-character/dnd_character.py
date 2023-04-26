ba747ad7d704027e3e14ecd548a89ead4a2f6021
Rodrigo Farah <digofarah@gmail.com>
2023-04-26 17:02:59 -0300

    exercise solved

--- python/dnd-character/dnd_character.py
+++ python/dnd-character/dnd_character.py
@@ -1,27 +1,28 @@
-"""Write a random character generator """
+"""
+Write a random Dungeons & Dragons character generator 
+
+Based on d10d's solution
+"""
 
 import random
 
 
-def score() -> int:
-    """Rolls four 6-sided dice and return the sum of the largest three dice"""
-    return sum(sorted([random.randint(1, 6) for n in range(4)])[1:])
+ABILITIES = ("strength", "dexterity", "constitution",
+             "intelligence", "wisdom", "charisma")
 
-ABILITIES = ("strenght", "dexterity", "constitution", "intelligence", "wisdom", "charisma")
-             
+
 def modifier(constitution_score: int) -> int:
     """Return Character's constitution modifier"""
     return (constitution_score - 10) // 2
 
+
 class Character:
     def __init__(self):
         for ability in ABILITIES:
-            setattr(self, ability, score())
-        
+            setattr(self, ability, self.ability())
+
         self.hitpoints = 10 + modifier(self.constitution)
 
-
-c1 = Character()
-
-
-print(c1.strenght, c1.dexterity, c1.constitution, c1.intelligence, c1.wisdom, c1.charisma)
+    def ability(self) -> int:
+        """Rolls four 6-sided dice and return the sum of the largest three dice"""
+        return sum(sorted([random.randint(1, 6) for n in range(4)])[1:])
