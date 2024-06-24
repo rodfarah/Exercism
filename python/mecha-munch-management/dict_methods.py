580db3d92b2490a6f5efab9d3629ef5d701af84f
Rodrigo Farah <digofarah@gmail.com>
2024-06-24 17:44:55 -0300

    uploads 'mecha munch management' exercise

--- python/mecha-munch-management/dict_methods.py
+++ python/mecha-munch-management/dict_methods.py
@@ -1,15 +1,22 @@
 """Functions to manage a users shopping cart items."""
 
 
-def add_item(current_cart, items_to_add):
+def add_item(current_cart: dict, items_to_add):
     """Add items to shopping cart.
 
     :param current_cart: dict - the current shopping cart.
     :param items_to_add: iterable - items to add to the cart.
     :return: dict - the updated user cart dictionary.
     """
+    for item in items_to_add:
+        if item not in current_cart.keys():
+            current_cart.update([(item, 1)])
+        else:
+            for product in current_cart.keys():
+                if product == item:
+                    current_cart[product] += 1
 
-    pass
+    return current_cart
 
 
 def read_notes(notes):
@@ -18,19 +25,34 @@ def read_notes(notes):
     :param notes: iterable of items to add to cart.
     :return: dict - a user shopping cart dictionary.
     """
+    order_dict = {}
 
-    pass
+    only_one = set(notes)
+
+    for item in only_one:
+        order_dict[item] = 0
+
+    for product in notes:
+        order_dict[product] += 1
+
+    return order_dict
 
 
 def update_recipes(ideas, recipe_updates):
     """Update the recipe ideas dictionary.
 
     :param ideas: dict - The "recipe ideas" dict.
-    :param recipe_updates: dict - dictionary with updates for the ideas section.
+    :param recipe_updates: dict - dictionary with updates for the ideas
+    section.
     :return: dict - updated "recipe ideas" dict.
     """
 
-    pass
+    for item in recipe_updates:
+        if item[0] in ideas.keys():
+            ideas[item[0]] = item[1]
+        else:
+            ideas.update([(item[0], item[1])])
+    return ideas
 
 
 def sort_entries(cart):
@@ -40,7 +62,11 @@ def sort_entries(cart):
     :return: dict - users shopping cart sorted in alphabetical order.
     """
 
-    pass
+    sorted_cart = {}
+    for item in sorted(cart.keys()):
+        sorted_cart.update([(item, cart[item])])
+
+    return sorted_cart
 
 
 def send_to_store(cart, aisle_mapping):
@@ -51,7 +77,13 @@ def send_to_store(cart, aisle_mapping):
     :return: dict - fulfillment dictionary ready to send to store.
     """
 
-    pass
+    bought_items = sorted(cart.keys(), reverse=True)
+    fulfillment_dictionary = {}
+    for item in bought_items:
+        fulfillment_dictionary.update(
+            [(item, [cart[item], aisle_mapping[item][0], aisle_mapping[item][1]])])
+
+    return fulfillment_dictionary
 
 
 def update_store_inventory(fulfillment_cart, store_inventory):
@@ -62,4 +94,10 @@ def update_store_inventory(fulfillment_cart, store_inventory):
     :return: dict - store_inventory updated.
     """
 
-    pass
+    for product, info in fulfillment_cart.items():
+        for item, details in store_inventory.items():
+            if item == product:
+                details[0] -= info[0]
+                if details[0] <= 0:
+                    details[0] = "Out of Stock"
+    return store_inventory
